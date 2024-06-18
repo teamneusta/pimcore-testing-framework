933635105085a881ffa2cbd2e8beb843fedd9625
Jacob Dreesen <j.dreesen@neusta.de>
2024-06-18 12:00:18 +0200

    Remove doctrine/annotations
    It is only used in the tests and should be included in `require-dev` at most.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,13 +2,10 @@
 
 declare(strict_types=1);
 
-use Doctrine\Common\Annotations\AnnotationRegistry;
 use Neusta\Pimcore\TestingFramework\Kernel\TestKernel;
 use Neusta\Pimcore\TestingFramework\Pimcore\BootstrapPimcore;
 
 include dirname(__DIR__) . '/vendor/autoload.php';
-
-AnnotationRegistry::registerLoader('class_exists');
 
 BootstrapPimcore::bootstrap(
     PIMCORE_PROJECT_ROOT: __DIR__ . '/app',
