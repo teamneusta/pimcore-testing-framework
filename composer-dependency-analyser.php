08f19b436ee58b38fc39ca455e0b4d6eefa90c3f
Jacob Dreesen <j.dreesen@neusta.de>
2024-08-12 13:15:26 +0200

    Check for composer dependency issues (#21)

---
+++ composer-dependency-analyser.php
@@ -0,0 +1,16 @@
+<?php
+
+use ShipMonk\ComposerDependencyAnalyser\Config\Configuration;
+use ShipMonk\ComposerDependencyAnalyser\Config\ErrorType;
+
+return (new Configuration())
+    // Exclude test app
+    ->addPathToExclude(__DIR__ . '/tests/app')
+
+    // Ignore optional dependency
+    ->ignoreErrorsOnPackageAndPath('dama/doctrine-test-bundle', __DIR__ . '/src/Database/ResetDatabase.php', [ErrorType::DEV_DEPENDENCY_IN_PROD])
+    ->ignoreErrorsOnPackageAndPath('dama/doctrine-test-bundle', __DIR__ . '/src/Database/DatabaseResetter.php', [ErrorType::DEV_DEPENDENCY_IN_PROD])
+
+    // Ignore a legacy class from Pimcore 10
+    ->ignoreUnknownClasses([Pimcore\Bundle\AdminBundle\PimcoreAdminBundle::class])
+;
