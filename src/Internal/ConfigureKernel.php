a354c9b12e43eb12180a6389f2ce57c468ad179d
Jacob Dreesen <j.dreesen@neusta.de>
2024-08-30 17:00:47 +0200

    Extract ConfigureKernel trait

---
+++ src/Internal/ConfigureKernel.php
@@ -0,0 +1,60 @@
+<?php
+declare(strict_types=1);
+
+namespace Neusta\Pimcore\TestingFramework\Internal;
+
+use Neusta\Pimcore\TestingFramework\Exception\DoesNotExtendKernelTestCase;
+use Neusta\Pimcore\TestingFramework\TestKernel;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+
+/**
+ * @mixin KernelTestCase
+ */
+trait ConfigureKernel
+{
+    /**
+     * @param array{config?: callable(TestKernel):void, environment?: string, debug?: bool, ...} $options
+     */
+    protected static function createKernel(array $options = []): TestKernel
+    {
+        if (!is_subclass_of(static::class, KernelTestCase::class)) {
+            throw DoesNotExtendKernelTestCase::forTrait(__TRAIT__);
+        }
+
+        $kernel = parent::createKernel($options);
+
+        if (!$kernel instanceof TestKernel) {
+            throw new \LogicException(\sprintf('Kernel must be an instance of %s', TestKernel::class));
+        }
+
+        KernelConfigurator::configure($kernel);
+
+        $kernel->handleOptions($options);
+
+        return $kernel;
+    }
+
+    /**
+     * @internal
+     *
+     * @before
+     */
+    public function _collectKernelConfigurations(): void
+    {
+        if (!$this instanceof KernelTestCase) {
+            throw DoesNotExtendKernelTestCase::forTrait(__TRAIT__);
+        }
+
+        KernelConfigurator::up($this);
+    }
+
+    /**
+     * @internal
+     *
+     * @after
+     */
+    public function _resetKernelConfigurations(): void
+    {
+        KernelConfigurator::down();
+    }
+}
--- src/KernelTestCase.php
+++ src/KernelTestCase.php
@@ -3,45 +3,9 @@ declare(strict_types=1);
 
 namespace Neusta\Pimcore\TestingFramework;
 
-use Neusta\Pimcore\TestingFramework\Internal\KernelConfigurator;
+use Neusta\Pimcore\TestingFramework\Internal\ConfigureKernel;
 
 abstract class KernelTestCase extends \Pimcore\Test\KernelTestCase
 {
-    /**
-     * @param array{config?: callable(TestKernel):void, environment?: string, debug?: bool, ...} $options
-     */
-    protected static function createKernel(array $options = []): TestKernel
-    {
-        $kernel = parent::createKernel($options);
-
-        if (!$kernel instanceof TestKernel) {
-            throw new \LogicException(sprintf('Kernel must be an instance of %s', TestKernel::class));
-        }
-
-        KernelConfigurator::configure($kernel);
-
-        $kernel->handleOptions($options);
-
-        return $kernel;
-    }
-
-    /**
-     * @internal
-     *
-     * @before
-     */
-    public function _collectKernelConfigurations(): void
-    {
-        KernelConfigurator::up($this);
-    }
-
-    /**
-     * @internal
-     *
-     * @after
-     */
-    public function _resetKernelConfigurations(): void
-    {
-        KernelConfigurator::down();
-    }
+    use ConfigureKernel;
 }
