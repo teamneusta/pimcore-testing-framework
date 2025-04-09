f830ee53d4e6d5e5a4ba7e0ec842f448adad8c7b
Jacob Dreesen <j.dreesen@neusta.de>
2025-04-09 14:19:03 +0200

    Add ConfigurableWebTestcase (#38)

---
+++ src/Test/ConfigurableWebTestcase.php
@@ -0,0 +1,75 @@
+<?php
+declare(strict_types=1);
+
+namespace Neusta\Pimcore\TestingFramework\Test;
+
+use Neusta\Pimcore\TestingFramework\Kernel\TestKernel;
+use Neusta\Pimcore\TestingFramework\Test\Attribute\KernelConfiguration;
+use PHPUnit\Framework\TestCase;
+use Pimcore\Test\WebTestCase;
+
+abstract class ConfigurableWebTestcase extends WebTestCase
+{
+    /** @var list<KernelConfiguration> */
+    private static iterable $kernelConfigurations = [];
+
+    /**
+     * @param array{config?: callable(TestKernel):void, environment?: string, debug?: bool, ...} $options
+     */
+    protected static function createKernel(array $options = []): TestKernel
+    {
+        $kernel = parent::createKernel($options);
+        \assert($kernel instanceof TestKernel);
+
+        foreach (self::$kernelConfigurations as $configuration) {
+            $configuration->configure($kernel);
+        }
+
+        $kernel->handleOptions($options);
+
+        return $kernel;
+    }
+
+    /**
+     * @internal
+     *
+     * @before
+     */
+    public function _getKernelConfigurationFromAttributes(): void
+    {
+        $class = new \ReflectionClass($this);
+        $method = $class->getMethod($this->getName(false));
+        $providedData = $this->getProvidedData();
+        $configurations = [];
+
+        foreach ($class->getAttributes(KernelConfiguration::class, \ReflectionAttribute::IS_INSTANCEOF) as $attribute) {
+            $configurations[] = $attribute->newInstance();
+        }
+
+        foreach ($method->getAttributes(KernelConfiguration::class, \ReflectionAttribute::IS_INSTANCEOF) as $attribute) {
+            $configurations[] = $attribute->newInstance();
+        }
+
+        if ([] !== $providedData) {
+            foreach ($providedData as $data) {
+                if ($data instanceof KernelConfiguration) {
+                    $configurations[] = $data;
+                }
+            }
+
+            // remove them from the arguments passed to the test method
+            (new \ReflectionProperty(TestCase::class, 'data'))->setValue($this, array_values(array_filter(
+                $providedData,
+                fn ($data) => !$data instanceof KernelConfiguration,
+            )));
+        }
+
+        self::$kernelConfigurations = $configurations;
+    }
+
+    protected function tearDown(): void
+    {
+        self::$kernelConfigurations = [];
+        parent::tearDown();
+    }
+}
