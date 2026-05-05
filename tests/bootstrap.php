688f95c66b1e5607680df30147fc5a5ce0fce5bb
Jacob Dreesen <j.dreesen@neusta.de>
2026-05-05 17:19:05 +0200

    fix: install `symfony/runtime` instead of registering Symfony's exception handler
    Just the availability of Symfony's runtime component fixes it, even if we don't use it anywhere ourselves.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -4,13 +4,8 @@ declare(strict_types=1);
 
 use Neusta\Pimcore\TestingFramework\Kernel\TestKernel;
 use Neusta\Pimcore\TestingFramework\Pimcore\BootstrapPimcore;
-use Symfony\Component\ErrorHandler\ErrorHandler;
 
 include dirname(__DIR__) . '/vendor/autoload.php';
-
-// This fixes PHPUnit's complaint: "Test code or tested code did not remove its own exception handlers"
-// See: https://github.com/symfony/symfony/issues/53812#issuecomment-1962740145
-set_exception_handler([new ErrorHandler(), 'handleException']);
 
 BootstrapPimcore::bootstrap(
     PIMCORE_PROJECT_ROOT: __DIR__ . '/app',
