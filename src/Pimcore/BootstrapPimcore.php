8ee5011b95f58b4c380fb2ee6e7f0490ac23877b
Jacob Dreesen <j.dreesen@neusta.de>
2026-07-01 14:35:28 +0200

    feat: load environment variables using Symfony Dotenv during Pimcore bootstrap (#50)

--- src/Pimcore/BootstrapPimcore.php
+++ src/Pimcore/BootstrapPimcore.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Neusta\Pimcore\TestingFramework\Pimcore;
 
 use Pimcore\Bootstrap;
+use Symfony\Component\Dotenv\Dotenv;
 
 final class BootstrapPimcore
 {
@@ -19,6 +20,7 @@ final class BootstrapPimcore
         }
 
         Bootstrap::setProjectRoot();
+        self::loadDotEnv();
         Bootstrap::bootstrap();
         AdminMode::disable();
     }
@@ -26,5 +28,20 @@ final class BootstrapPimcore
     public static function setEnv(string $name, string $value): void
     {
         putenv("{$name}=" . $_ENV[$name] = $_SERVER[$name] = $value);
+    }
+
+    private static function loadDotEnv(): void
+    {
+        if (isset($_SERVER['SYMFONY_DOTENV_VARS'])) {
+            return;
+        }
+
+        if (!class_exists(Dotenv::class)) {
+            return;
+        }
+
+        if (file_exists($filename = PIMCORE_PROJECT_ROOT . '/.env')) {
+            (new Dotenv())->bootEnv($filename);
+        }
     }
 }
