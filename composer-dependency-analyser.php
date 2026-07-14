9f37520947dfbccd84492e246d02753b082210ac
Jacob Dreesen <j.dreesen@neusta.de>
2026-07-14 19:19:56 +0200

    chore: consolidate dependency analyzer configuration for Pimcore version-specific handling

--- composer-dependency-analyser.php
+++ composer-dependency-analyser.php
@@ -1,9 +1,10 @@
 <?php
 
+use Neusta\Pimcore\TestingFramework\Pimcore\PlatformVersion;
 use ShipMonk\ComposerDependencyAnalyser\Config\Configuration;
 use ShipMonk\ComposerDependencyAnalyser\Config\ErrorType;
 
-return (new Configuration())
+$config = (new Configuration())
     // Exclude test app
     ->addPathToExclude(__DIR__ . '/tests/app')
 
@@ -17,7 +18,10 @@ return (new Configuration())
     ->ignoreErrorsOnPackageAndPath('doctrine/dbal', __DIR__ . '/src/Database/PlatformDatabaseInstaller.php', [ErrorType::DEV_DEPENDENCY_IN_PROD])
     // pimcore/admin-ui-classic-bundle is optional on every supported Pimcore version,
     // so its class is unresolvable rather than merely a "dev dependency in prod".
-    ->ignoreUnknownClasses(['Pimcore\Bundle\AdminBundle\PimcoreAdminBundle'])
-    // Only exists on Pimcore ^2026.1 - see composer-dependency-analyser-2026.php, used for that leg.
-    ->ignoreUnknownClasses(['Pimcore\Bundle\InstallBundle\Database\DatabaseSetup'])
-;
+    ->ignoreUnknownClasses(['Pimcore\Bundle\AdminBundle\PimcoreAdminBundle']);
+
+if (PlatformVersion::getMajor() < 2026) {
+    $config->ignoreUnknownClasses(['Pimcore\Bundle\InstallBundle\Database\DatabaseSetup']);
+}
+
+return $config;
